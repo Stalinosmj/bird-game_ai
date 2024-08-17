cb9ad491ba632359cfe368da77b54cd95fac3c04
Stalinosmj <stalinosmj@outlook.com>
2024-08-17 15:28:11 -0400

    implemented network,layer and neuron

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -4,8 +4,10 @@ pub struct Network {
 }
 
 impl Network {
-    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {  //Numbers have to be inserted into each layer
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
 }
 
@@ -15,8 +17,11 @@ struct Layer {
 }
 
 impl Layer {
-    fn propagate(&self , inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> { //A single neuron accepts many inputs and returns a single output
+        self.neurons
+        .iter()
+        .map(|neuron| neuron.propagate(&inputs))
+        .collect()
     }
 }
 
@@ -26,3 +31,8 @@ struct Neuron {
     weights: Vec<f32>, //Neurons conatins bias and weights
 }
 
+impl Neuron {
+    fn propagate(&self , inputs: &[f32]) -> f32 {
+        todo!()
+    }
+}
